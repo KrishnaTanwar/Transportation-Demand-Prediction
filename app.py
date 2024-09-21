1b6db0364c4ef3f612cc96dbc3bb41583618f529
Krishna Tanwar <142567194+KrishnaTanwar@users.noreply.github.com>
2024-09-21 16:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ from PIL import Image
 
 image = Image.open("2.png")
 # Resize the image while maintaining the aspect ratio
-image = image.resize((3000, 1000))  # Adjust the width and height as needed
+image = image.resize((3000, 100))  # Adjust the width and height as needed
 st.image(image)
 
 
